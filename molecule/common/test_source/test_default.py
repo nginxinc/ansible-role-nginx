91cc75d9ebd85d5e4b2acd58f78f6d2993c4f2df
Alessandro Fael Garcia <alessfg@hotmail.com>
2020-03-24 15:18:21 +0100

    Add service manager to Debian images

--- molecule/common/test_source/test_default.py
+++ molecule/common/test_source/test_default.py
@@ -6,6 +6,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def test_nginx_running_and_enabled(host):
+    ngx = host.service("nginx")
+    assert ngx.is_running
+    assert ngx.is_enabled
+
+
 def test_hosts_file(host):
     ngx = host.file('/etc/hosts')
     assert ngx.exists
